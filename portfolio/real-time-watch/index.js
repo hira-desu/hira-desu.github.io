255618769b9eb862f8e43542c5c95f05df5eb67a
hira-desu <77167443+hira-desu@users.noreply.github.com>
2021-01-09 04:20:07 +0900

    Update index.js

--- portfolio/real-time-watch/index.js
+++ portfolio/real-time-watch/index.js
@@ -1,6 +1,17 @@
-var nowTime = new Date(); //  現在日時を得る
-var nowHour = nowTime.getHours(); // 時を抜き出す
-var nowMin  = nowTime.getMinutes(); // 分を抜き出す
-var nowSec  = nowTime.getSeconds(); // 秒を抜き出す
-var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-document.getElementById("RealtimeClockArea").innerHTML = msg;
+
+function set2fig(num) {
+   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+   var ret;
+   if( num < 10 ) { ret = "0" + num; }
+   else { ret = num; }
+   return ret;
+}
+function showClock2() {
+   var nowTime = new Date();
+   var nowHour = set2fig( nowTime.getHours() );
+   var nowMin  = set2fig( nowTime.getMinutes() );
+   var nowSec  = set2fig( nowTime.getSeconds() );
+   var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+   document.getElementById("RealtimeClockArea2").innerHTML = msg;
+}
+setInterval('showClock2()',1000);
